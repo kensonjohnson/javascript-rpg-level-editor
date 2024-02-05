58cffd2ff24974256411a4554a0fe83d9b9d1272
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-04 18:05:06 -0600

    Add TilesetLoader and convert to client component

--- components/editing/AppLayout.tsx
+++ components/editing/AppLayout.tsx
@@ -1,20 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./AppLayout.module.css";
 
+import { TilesetLoader } from "../tilesets/TilesetLoader";
+
 interface AppLayoutProps {}
 
-export default function AppLayout(props: AppLayoutProps) {
+export function AppLayout(props: AppLayoutProps) {
   return (
-    <main className={styles.appLayout}>
-      <header className={styles.header}>
-        <div className={styles.titleContainer}>
-          <h1>Map Title</h1>
-          <Link href="/maps">All Maps</Link>
-        </div>
-        <div>Save Button</div>
-      </header>
-      <aside className={styles.aside}>Tile Set Here</aside>
-      <div className={styles.workingArea}>Painting Area Here</div>
-    </main>
+    <TilesetLoader
+      renderLoading={() => {
+        return <p>Loading...</p>;
+      }}
+      renderLoaded={() => {
+        return (
+          <main className={styles.appLayout}>
+            <header className={styles.header}>
+              <div className={styles.titleContainer}>
+                <h1>Map Title</h1>
+                <Link href="/maps">All Maps</Link>
+              </div>
+              <div>Save Button</div>
+            </header>
+            <aside className={styles.aside}>Tile Set Here</aside>
+            <div className={styles.workingArea}>Painting Area Here</div>
+          </main>
+        );
+      }}
+    />
   );
 }
