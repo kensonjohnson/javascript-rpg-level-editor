067414b6da9618a9842db1dde0645cd357d63490
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-05 21:04:56 -0600

    Setup parsing of  json files

--- app/maps/[fileName]/page.tsx
+++ app/maps/[fileName]/page.tsx
@@ -1,5 +1,22 @@
 import { AppLayout } from "@/components/editing/AppLayout";
+import { readMapJsonFile } from "./readMapJsonFile";
+import { redirect } from "next/navigation";
 
-export default function MapDetailsPage() {
-  return <AppLayout />;
+type MapDetailsPageProps = {
+  params: {
+    fileName: string;
+  };
+};
+
+export default async function MapDetailsPage({ params }: MapDetailsPageProps) {
+  const fileName = params.fileName;
+  const initialData = await readMapJsonFile(fileName);
+  console.log(initialData);
+
+  // Redirect to maps page if no file found
+  if (!initialData) {
+    redirect("/maps");
+  }
+
+  return <AppLayout fileName={fileName} initialData={initialData} />;
 }
