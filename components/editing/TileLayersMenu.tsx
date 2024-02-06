883bfd164530b236159beafaae33ca71147ea2ba
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-05 19:32:12 -0600

    Create menu for layer switching

---
+++ components/editing/TileLayersMenu.tsx
@@ -0,0 +1,58 @@
+import { LAYER_BOTTOM, LAYER_TOP } from "@/classes/TilePlacement";
+import styles from "./TileLayersMenu.module.css";
+import { IconClosedEye } from "../icons/IconClosedEye";
+import { IconEye } from "../icons/IconEye";
+import { useContext } from "react";
+import { TileLayersContext } from "@/contexts/TileLayersContext";
+
+type TileLayersMenuProps = {};
+
+export function TileLayersMenu({}: TileLayersMenuProps) {
+  const [layersState, setLayersState] = useContext(TileLayersContext);
+
+  return (
+    <div className={styles.tileLayerMenu}>
+      {Object.entries(layersState.layers).map(([layerName, layer]) => {
+        const isActive = layerName === layersState.currentLayer;
+        const isVisible = layer.visible;
+
+        return (
+          <div
+            className={styles.layerRow}
+            key={layerName}
+            data-active={isActive}
+          >
+            <button
+              onClick={() => {
+                setLayersState({
+                  ...layersState,
+                  layers: {
+                    ...layersState.layers,
+                    [layerName]: {
+                      ...layersState.layers[layerName],
+                      visible: !layersState.layers[layerName].visible,
+                    },
+                  },
+                });
+              }}
+              data-visible={isVisible}
+            >
+              {isVisible ? <IconEye /> : <IconClosedEye />}
+            </button>
+            <button
+              onClick={() => {
+                setLayersState({
+                  ...layersState,
+                  currentLayer: layerName,
+                });
+              }}
+              className={styles.layerName}
+            >
+              {layer.name}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
