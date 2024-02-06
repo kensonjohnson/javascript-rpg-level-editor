59a97cc4dfe3fbaf1e2c735733e0b5c9ce8ea5fb
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-05 19:32:26 -0600

    Hook up menu selections with layers

--- components/editing/LayeredDrawingCanvases.tsx
+++ components/editing/LayeredDrawingCanvases.tsx
@@ -6,6 +6,8 @@ import {
 import { TilesetImageMap } from "../tilesets/TilesetLoader";
 import { DrawingCanvas } from "./DrawingCanvas";
 import styles from "./LayeredDrawingCanvases.module.css";
+import { useContext } from "react";
+import { TileLayersContext } from "@/contexts/TileLayersContext";
 
 type LayeredDrawingCanvasesProps = {
   tilesetPlacementsRef: TilePlacements;
@@ -16,6 +18,8 @@ export function LayeredDrawingCanvases({
   tilesetPlacementsRef,
   tilesetImageMap,
 }: LayeredDrawingCanvasesProps) {
+  const [layersState] = useContext(TileLayersContext);
+
   return (
     <div
       className={styles.layeredDrawingCanvases}
@@ -24,14 +28,22 @@ export function LayeredDrawingCanvases({
         height: tilesetPlacementsRef.height * 2,
       }}
     >
-      <div className={styles.layeredCanvasContainer}>
+      <div
+        data-layer-visible={layersState.layers[LAYER_BOTTOM].visible}
+        data-accept-pointer-events={layersState.currentLayer === LAYER_BOTTOM}
+        className={styles.layeredCanvasContainer}
+      >
         <DrawingCanvas
           layerId={LAYER_BOTTOM}
           tilePlacementsRef={tilesetPlacementsRef}
           tilesetImageMap={tilesetImageMap}
         />
       </div>
-      <div className={styles.layeredCanvasContainer}>
+      <div
+        data-layer-visible={layersState.layers[LAYER_TOP].visible}
+        data-accept-pointer-events={layersState.currentLayer === LAYER_TOP}
+        className={styles.layeredCanvasContainer}
+      >
         <DrawingCanvas
           layerId={LAYER_TOP}
           tilePlacementsRef={tilesetPlacementsRef}
