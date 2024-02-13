11c3b26329b246efdaaf14977aa6a7f7e83484a1
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-12 20:34:41 -0600

    Add selecting of objects

--- components/editing/ObjectCanvasPlacement.tsx
+++ components/editing/ObjectCanvasPlacement.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import styles from "./ObjectCanvasPlacement.module.css";
+import { ObjectPlacementsContext } from "@/contexts/ObjectPlacementsContext";
 
 type ObjectCanvasPlacementProps = {
   id: string;
@@ -8,12 +10,17 @@ type ObjectCanvasPlacementProps = {
 };
 
 export function ObjectCanvasPlacement(props: ObjectCanvasPlacementProps) {
+  const [objectPlacements, setObjectPlacements] = useContext(
+    ObjectPlacementsContext
+  );
+
   let src = "/avatars/hero-avatar.png";
   if (props.type === "NPC") {
     src = "/avatars/npc-avatar.png";
   }
 
   const SCALE = 2;
+  const isActive = objectPlacements.activeObject === props.id;
 
   return (
     <button
@@ -21,9 +28,13 @@ export function ObjectCanvasPlacement(props: ObjectCanvasPlacementProps) {
       style={{
         left: props.x * SCALE,
         top: props.y * SCALE,
+        outline: isActive ? "2px solid cyan" : "none",
       }}
       onClick={() => {
-        console.log("ObjectCanvasPlacement clicked");
+        setObjectPlacements({
+          ...objectPlacements,
+          activeObject: props.id,
+        });
       }}
     >
       <img src={src} alt={props.type} className={styles.placementImg} />
