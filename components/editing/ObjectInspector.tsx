52459d76e7dd22613735970a78a7b30eff44942c
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-12 20:51:26 -0600

    Add buttons to move or remove objects from screen

--- components/editing/ObjectInspector.tsx
+++ components/editing/ObjectInspector.tsx
@@ -12,9 +12,27 @@ export function ObjectInspector() {
     return;
   }
 
+  function modifyPosition(xChange: number, yChange: number) {
+    const newObjects = objectPlacements.objects.map((obj) => {
+      if (obj.id === objectPlacements.activeObject) {
+        return {
+          ...obj,
+          x: obj.x + xChange,
+          y: obj.y + yChange,
+        };
+      }
+      return obj;
+    });
+    setObjectPlacements({
+      ...objectPlacements,
+      objects: newObjects,
+    });
+  }
+
   return (
     <div className={styles.objectInspectorPane}>
       <button
+        className="button secondary"
         onClick={() => {
           setObjectPlacements({
             ...objectPlacements,
@@ -25,6 +43,57 @@ export function ObjectInspector() {
         Close
       </button>
       <p>Now Editing: {objectPlacements.activeObject}</p>
+      <div className={styles.positionButtonsContainer}>
+        <button
+          className="button secondary"
+          onClick={() => {
+            modifyPosition(0, -16);
+          }}
+        >
+          UP
+        </button>
+        <button
+          className="button secondary"
+          onClick={() => {
+            modifyPosition(0, 16);
+          }}
+        >
+          DOWN
+        </button>
+        <button
+          className="button secondary"
+          onClick={() => {
+            modifyPosition(-16, 0);
+          }}
+        >
+          LEFT
+        </button>
+        <button
+          className="button secondary"
+          onClick={() => {
+            modifyPosition(16, 0);
+          }}
+        >
+          RIGHT
+        </button>
+      </div>
+      <div>
+        <button
+          className="button danger"
+          onClick={() => {
+            const newObjects = objectPlacements.objects.filter(
+              (obj) => obj.id !== objectPlacements.activeObject
+            );
+            setObjectPlacements({
+              ...objectPlacements,
+              objects: newObjects,
+              activeObject: null,
+            });
+          }}
+        >
+          Remove
+        </button>
+      </div>
     </div>
   );
 }
